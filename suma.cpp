aba224c90c5e46246c258315592ffcac013ef907
Franfmga2022 <francisco.garcia.alcivar@utelvt.edu.ec>
2022-09-20 19:03:12 -0500

    se adiciono el programa suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float x,y,z;
+	cout<<"Ingrese 2 numeros x y :";
+	cin>>x>>y;
+	z=x+y;
+	cout<<"El resultado es :";
+	cout<<z;
+	return 0;
+}
+
