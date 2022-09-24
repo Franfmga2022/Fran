fbef38624695a076f5f50a60368b86d3fdef2cd5
Franfmga2022 <francisco.garcia.alcivar@utelvt.edu.ec>
2022-09-24 15:41:39 -0500

    agregue el programa velocimetro

--- velocimetro.cpp
+++ velocimetro.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    float v,d,t;
-    cout<<"Ingrese la distancia (en km) d= "; cin>>d;
-    cout<<"Ingrese el tiempo (en horas)  t= "; cin>>t;
-    v=d/t;
-    cout<<"La velocidad calculada es de v= "<<t<<endl<<endl;
+    float V_v,V_d,V_t;
+    cout<<"Ingrese la distancia (en km) V_d= "; cin>>V_d;
+    cout<<"Ingrese el tiempo (en horas)  V_t= "; cin>>V_t;
+    V_v=V_d/V_t;
+    cout<<"La velocidad calculada es de V_v= "<<V_v<<endl<<endl;
     return 0;
 
 
